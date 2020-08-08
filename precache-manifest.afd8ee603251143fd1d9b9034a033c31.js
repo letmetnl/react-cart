4b5df8065b352bb3aee07a9ab48e118b5aa44232
mohits Projects <mohit5150515@gmail.com>
2020-08-08 19:25:42 +0530

    Updates

---
+++ precache-manifest.afd8ee603251143fd1d9b9034a033c31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c95744274cdd3330f83173a53c8293e4",
+    "url": "/react-cart/index.html"
+  },
+  {
+    "revision": "0420899ff9aafc89134a",
+    "url": "/react-cart/static/css/main.07d1766a.chunk.css"
+  },
+  {
+    "revision": "3708e005db06ad099794",
+    "url": "/react-cart/static/js/2.de39d74d.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/react-cart/static/js/2.de39d74d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0420899ff9aafc89134a",
+    "url": "/react-cart/static/js/main.1ad68b8a.chunk.js"
+  },
+  {
+    "revision": "d5374704157d20a2a052",
+    "url": "/react-cart/static/js/runtime-main.7bb402ca.js"
+  }
+]);
